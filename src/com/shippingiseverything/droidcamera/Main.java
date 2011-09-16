00be56687fe784f643a9407e330cde12887656cf
Sumeet Parmar <z.sumeet@gmail.com>
2011-09-15 17:14:13 -0700

    wip

--- src/com/shippingiseverything/droidcamera/Main.java
+++ src/com/shippingiseverything/droidcamera/Main.java
@@ -1,7 +1,9 @@
 package com.shippingiseverything.droidcamera;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main extends Activity {
 	
@@ -9,6 +11,10 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Drawable d = new BrushedMetallicDrawable();
+        View view = findViewById(R.id.rl_top_header);
+        view.setBackgroundDrawable(d);
     }
     
 }
